cc2d4fe65db29ced774c66c516eda4ad7cd41dab
Avishai Peretz <avishai@AvishaiP-MAC.local>
2022-10-25 10:00:56 +0300

    chor: rabbitMQ initialize but not implemented.

--- index.ts
+++ index.ts
@@ -4,7 +4,6 @@ import bodyParser from 'body-parser';
 import orders from './routes/orders';
 import root from './routes/root';
 import { createDB } from './db';
-import amqpSender from './middlewares/rabbitmq/sender';
 
 dotenv.config();
 const PORT = +process.env.PORT! || 5050
@@ -17,6 +16,7 @@ app.use(express.json({ limit: "30mb" }));
 app.use(bodyParser.urlencoded({extended: false}));
 app.use('/', root)
 app.use('/orders', orders );
+
 
 app.listen(PORT, () => console.log("SERVER IS UP AND RUNNING ON PORT ", PORT))
   .on("error", (err) => console.log('Error', err));
